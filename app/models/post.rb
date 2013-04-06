f0ab7181e9dab919a400f49a697cbeb1ed831074
harshar <harsha.harsharajan@gmail.com>
2013-04-06 17:04:07 +0530

    view changes for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  attr_accessible  :post
+  attr_accessible  :post,:user_id
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130406083530) do
+ActiveRecord::Schema.define(:version => 20130406111541) do
 
   create_table "posts", :force => true do |t|
     t.text     "post"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
